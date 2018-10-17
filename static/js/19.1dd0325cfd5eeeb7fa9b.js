69074ae630910c5d8d19633301f024ae2011da84
areve <areve@example.com>
2018-10-17 19:28:36 +0100

    update site

--- static/js/19.1dd0325cfd5eeeb7fa9b.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([19],{HHZl:function(e,t){},"qt+6":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("section",[t("h1",[this._v("Notepad")])])}]};var a=n("VU/8")({name:"Notepad",data:function(){return{}}},r,!1,function(e){n("HHZl")},"data-v-117fe7d3",null);t.default=a.exports}});
-//# sourceMappingURL=19.1dd0325cfd5eeeb7fa9b.js.map
